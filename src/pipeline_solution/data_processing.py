8234bba478c5c2e03cb005e802f272088e1029b3
Sebastian Sarasti <sebitas.alejo@hotmail.com>
2024-07-19 08:56:20 -0500

    solucion final

--- src/pipeline_solution/data_processing.py
+++ src/pipeline_solution/data_processing.py
@@ -7,7 +7,7 @@ from utils import calculate_statiscal_descriptors, remove_outliers, chi2_test, a
 
 from sklearn.preprocessing import LabelEncoder
 
-#from phik import phik_matrix
+from phik import phik_matrix
 
 
 def data_loading(path):
@@ -109,23 +109,11 @@ def outliers_treatment(df):
     return df, num_cols 
 
 def feature_engineering(df):
-    """df = df.query("duration_ms >=10")
-    df["prom1"] = df[["danceability", "instrumentalness"]].mean(axis=1)
-    df["prom2"] = df[["energy", "loudness"]].mean(axis=1)
-    df["prom3"] = df[["energy", "liveness"]].mean(axis=1)
-    encoders = {}
-    cat_cols = ["key", "mode", "music_genre"]
-    for col in cat_cols:
-        le = LabelEncoder()
-        df[col] = le.fit_transform(df[[col]])
-        encoders[col] = le
-    return df"""
-    df["prom_viento"] = df[["Wind-Speed U","Wind-Speed V"]].mean(axis=1)
-    df["ratio_viento"] = df["Wind-Speed U"]/df["Wind-Speed V"]
+    df["prom_viento"] = df[["Wind-Speed (U)","Wind-Speed (V)"]].mean(axis=1)
+    df["ratio_viento"] = df["Wind-Speed (U)"]/df["Wind-Speed (V)"]
     df["prom_temp"] = df[["Soil Temp", "Dewpoint Temp","Temp"]].mean(axis=1)
     df["ratio_NO2_O3"] = df["O3"]/df["NO2"]
     df["prom_NO2_O3"] = df["O3"]*df["NO2"]
-    #df["prom_NO2_wind"] = df["O3"]*df["prom_viento"]
     df["Prec_Humedad"] = df["Total Percipitation"]*df["Relative Humidity"]
     df["location"] = df[["Latitude", "Longitude"]].mean(axis=1)
     df["Year_time"] = df[["month", "season"]].mean(axis=1)
@@ -134,22 +122,14 @@ def feature_engineering(df):
 
 
 
-def feature_selection(df, num_cols):
-    #cat_cols = ["key", "mode"]
-    useful_var = []
-    # aplicamos anova
-    for col in num_cols:
-        p = anova_test(df, col, "PM10")
-        if p < 0.05:
-            useful_var.append(col)
-    """# segundo voy a hacer el chi2
-    for col in cat_cols:
-        p = chi2_test(df, col, "music_genre")
-        if p < 0.05:
-            useful_var.append(col)"""
-    df = df[useful_var + ["PM10"]]
+def feature_selection(df):
     matrix_phik = df.phik_matrix()
-    final_var = list(matrix_phik.query("PM10 > 0.22").index)
+    final_var_PM10 = list(matrix_phik.query("PM10 >= 0.25").index)
+    final_var_PM25 = list(matrix_phik.query("`PM2.5` >= 0.25").index)
+    final_var = final_var_PM10
+    for var in final_var_PM25:
+        if var not in final_var:
+            final_var.append(var)
     df_final = df[final_var]
     return df_final
     
